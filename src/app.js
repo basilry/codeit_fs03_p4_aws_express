3df2f4bd2a1f8cbeb755147d7050cc9e362f9615
basilry <basilry@gmail.com>
2025-02-04 21:57:41 +0900

    [update] fix

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000!');
+  console.log('Server is running on http://localhost:3000!!');
 });
 
 app.get('/', (req, res) => {
