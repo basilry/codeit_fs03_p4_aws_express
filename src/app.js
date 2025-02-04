ce8f414197e2ccbdc60171f9897e5751dbf9edd4
basilry <basilry@gmail.com>
2025-02-04 21:50:16 +0900

    [init] app.js 생성

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// console.log('Hello, AWS!');
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, AWS!');
+});
